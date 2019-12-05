7ee5074d8bc724a12b3c1696519dc777d292acc2
sbibek <sbibek@hotmail.com>
2019-12-04 18:51:02 -0800

    changes

--- main.cpp
+++ main.cpp
@@ -5,6 +5,17 @@ using namespace std;
 using namespace boost::asio;
 
 int main() {
-    cout << "c++";
+    cout << "here";
+    io_service io_service;
+    ip::tcp::acceptor acceptor(io_service, ip::tcp::endpoint(ip::tcp::v4(), 2001));
+    for(;;){
+        cout << "somebody connected";
+        ip::tcp::socket socket(io_service);
+        acceptor.accept(socket);
+        boost::system::error_code ie;
+        write(socket, boost::asio::buffer("test"), ie);
+        std::flush(std::cout);
+    }
+
     return 0;
 }
